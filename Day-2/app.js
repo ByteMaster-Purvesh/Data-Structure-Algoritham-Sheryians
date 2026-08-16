1f8c9efb16b8b4031afee003243fb4e116b9046d
ByteMaster-Purvesh <somwanshipurvesh@gmail.com>
2026-08-16 16:02:25 +0530

    Number Reversal

--- Day-2/app.js
+++ Day-2/app.js
@@ -17,6 +17,19 @@ console.log('\n Chaturyug: ', chaturyug)
 console.log(' Manvanter: ', manvanter)
 
 
+let a = 250
+let b = 360
+
+console.log('\n Value of A : ', a) 
+console.log(' Value of B : ', b) 
+
+let sum = a + b
+console.log('\n Sum : ', sum)
+
+a = sum - a
+b = sum - b
+console.log('\n Value of A : ', a) 
+console.log(' Value of B : ', b) 
 
 
 // let chaturyug = '43,20,000'
