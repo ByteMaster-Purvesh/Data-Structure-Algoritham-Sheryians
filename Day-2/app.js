25373cc94fcd40d5fa0b0e8888858b2b6df66da0
ByteMaster-Purvesh <somwanshipurvesh@gmail.com>
2026-08-16 15:52:59 +0530

    String Reversal

---
+++ Day-2/app.js
@@ -0,0 +1,38 @@
+let chaturyug = '43,20,000'
+let manvanter = '30,67,20,000'
+let temp
+
+console.log('\n Original value of Yugas')
+
+console.log('\n Chaturyug: ', chaturyug)
+console.log(' Manvanter: ', manvanter)
+
+// temp container 
+temp = manvanter
+manvanter =  chaturyug
+chaturyug = temp
+
+console.log('\n Reversal value of Yugas')
+console.log('\n Chaturyug: ', chaturyug)
+console.log(' Manvanter: ', manvanter)
+
+
+
+
+// let chaturyug = '43,20,000'
+// let manvanter = '30,67,20,000'
+// let temp
+
+// console.log('\n Original value of Yugas')
+
+// console.log('\n Chaturyug: ', chaturyug)
+// console.log(' Manvanter: ', manvanter)
+
+// // temp container 
+// temp = manvanter
+// manvanter =  chaturyug
+// chaturyug = temp
+
+// console.log('\n Reversal value of Yugas')
+// console.log('\n Chaturyug: ', chaturyug)
+// console.log(' Manvanter: ', manvanter)
